441393156d159f7d1ae6a56b25baa2d09de917fc
jessehirscho <jesse8477@gmail.com>
2025-03-28 23:05:16 +1100

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,33 @@
+import time
+import pandas 
+
+
+hotelsLs = []
+# Loop through hotels
+print("Checkpoint #1")
+
+# while len(hotelsLs) <>
+    # listings = 
+    print("Checkpoint #2")
+
+    for listing in listings:
+        try:
+            title = 
+            address = 
+            room_type =
+            price = 
+            review_score = 
+            num_reviews =
+
+            hotelsLs.append([title, address, room_type, price, review_score, num_reviews])
+        except:
+            continue
+
+
+
+# CSV functionality
+data 
+filename = 'test.csv'
+print("Checkpoint #3")
+data.convertToCSV(filename, index=False)
+print("Data officialy scraped!")
