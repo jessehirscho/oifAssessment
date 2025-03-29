7feeada19d364096de52bc43e98c3ef6acfe1bc8
jessehirscho <jesse8477@gmail.com>
2025-03-30 00:11:08 +1100

    Update bookingsAPI.py

--- bookingsAPI.py
+++ bookingsAPI.py
@@ -2,3 +2,12 @@ import flask
 import pandas 
 
 app = flask()
+
+testStr = "String"
+filename = 'test.csv'
+
+# Load data from csv 
+def load_date():
+
+    csv = panda.read_csv(filename)
+    return testStr
