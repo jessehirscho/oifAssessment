31b1c68aa9ae86126a2c533923a2b98e9ddad90b
jessehirscho <jesse8477@gmail.com>
2025-03-31 09:03:50 +1100

    API endpoint built
    Root URL working

--- bookingsAPI.py
+++ bookingsAPI.py
@@ -1,14 +1,48 @@
-import flask
-import pandas 
+import csv
+from flask import Flask, jsonify
 
-app = flask()
 
 testStr = "String"
 filename = 'test.csv'
 
-# MAKE API GET CALL
-# Load data from csv 
-def load_date():
+app = Flask(__name__)
 
-    csv = panda.read_csv(filename)
-    return testStr
+# Route for the root URL
+@app.route('/', methods=['GET'])
+def index():
+    return "Welcome to the Booking API!"
+
+
+# MAKE API GET CALL + Load data from csv 
+@app.route('/cheapest-listings', methods=['GET'])
+def load_data():
+    listings = []
+
+    try:
+         with open("listings.csv", "r") as csv_file:
+            lines = csv.dictReader(csv_file)
+            for line in lines:
+                listings.append(line)
+    except FileNotFoundError:
+        return jsonify({"Err: File not exists"}), 404
+    
+    for listing in listings:
+        print(listing)
+        try:
+            listing["Cost (AUD)"] = float(listing["Cost (AUD)"].replace(',', ''))
+        except ValueError:
+            listing["Cost (AUD)"] = float('inf')
+        
+        for listing in cheapest_listings:
+            if listing["Cost (AUD)"] != float('inf'):
+                listing["Cost (AUD)"] = str(listing["Cost (AUD)"])
+            else:
+                listing["Cost (AUD)"] = "NA"
+        cheapest_listings = sorted(listings, key=lambda x: x["Cost (AUD)"])[:50]
+
+
+    return jsonify(cheapest_listings)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
