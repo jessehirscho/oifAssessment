d4efe1fa8d6ef3f24dffb66941dd5a57361397cb
jessehirscho <jesse8477@gmail.com>
2025-03-30 23:36:10 +1100

    Added tests

--- scrape.py
+++ scrape.py
@@ -1,9 +1,6 @@
 import time
-import pandas 
-import requests
 import csv
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
@@ -11,7 +8,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 # SCRAPE LISTINGS
 def scrape_listings(csv_out="listings.csv"):
-    hotelsLs = []
+    hotels = []
     
     ## GET BOOKING.COM REQUEST
     search_url = "https://www.booking.com/searchresults.en-gb.html?ss=Australia&checkin_year=2026&checkin_month=02&checkin_monthday=01&checkout_year=2026&checkout_month=02&checkout_monthday=02&group_adults=1&no_rooms=1&group_children=0&sb_lp=1"
@@ -26,46 +23,56 @@ def scrape_listings(csv_out="listings.csv"):
     web_driver = webdriver.Chrome(options=selenium_instance)
     web_driver.get(search_url)
     
+    i = 0
     try: 
-        while True:
+        while i < 10000:
             listings = web_driver.find_elements(By.CLASS_NAME, "sr_property_block")
 
             for listing in listings:
                 try:
-                    title = listing.find_element(By.CLASS_NAME, "sr-hotel__name").text
-                    address = listing.find_element(By.CLASS_NAME, "sr_card_address_line").text
-                    room_type = listing.find_element(By.CLASS_NAME, "room_link").text
-                    price = listing.find_element(By.CLASS_NAME, "bui-price-display__value").text
-                    review_score = listing.find_element(By.CLASS_NAME, "bui-review-score__badge").text
-                    num_reviews = listing.find_element(By.CLASS_NAME, "bui-review-score__text").text
+                    title = listing.find_element(By.CLASS_NAME, "sr-hotel__name").text.strip()
+                    address = listing.find_element(By.CLASS_NAME, "sr_card_address_line").text.strip()
+                    room_type = listing.find_element(By.CLASS_NAME, "room_link").text.strip()
+                    price = listing.find_element(By.CLASS_NAME, "bui-price-display__value").text.strip()
+                    review_score = listing.find_element(By.CLASS_NAME, "bui-review-score__badge").text.strip()
+                    num_reviews = listing.find_element(By.CLASS_NAME, "bui-review-score__text").text.split(" ")[0]
 
-                    hotelsLs.append([title, address, room_type, price, review_score, num_reviews])
-                except NoSuchElementException:
-                    print("Click element not found")
+                    hotels.append({"title": title, 
+                                    "address": address, 
+                                    "room_type": room_type, 
+                                    "price": price, 
+                                    "review_score": review_score, 
+                                    "num_reviews": num_reviews})
+                    print(hotels)
+                    
+                except NoSuchElementException as e:
+                    print(f"Click element not found: {e}")
 
                     try:
                         next_button = web_driver.find_element(By.CLASS_NAME, "pagenext")
                         next_button.click()
                         time.sleep(3)
+                        i += 1
                     
-                    except NoSuchElementException:
-                        print("No more pages ")
+                    except NoSuchElementException as e:
+                        print(f"No more pages {e}")
                         break
-                    except TimeoutException:
-                        print("Timeout!!!!")
+                    except TimeoutException as e:
+                        print(f"Timeout!!!! {e}")
                         break
            
     except Exception as e:
         print(f"Err: {e}")
     finally:
+        print(i)
         web_driver.quit()
 
-    write_to_csv(hotelsLs, csv_out)
+    write_to_csv(hotels, csv_out)
 
 
 def write_to_csv(listings, csv_out):
     try:
-        with open(csv_out, "w") as csv_file:
+        with open(csv_out, "w", newline="") as csv_file:
             fieldnames = ["title", "address", "room_type", "price", "review_score", "num_reviews"]
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
             writer.writeheader()
