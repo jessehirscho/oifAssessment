2809e5cbf911aa4bf54da6a3c97df4c3adee69fb
jessehirscho <jesse8477@gmail.com>
2025-03-31 09:45:53 +1100

    working complete solution

--- bookingsAPI.py
+++ bookingsAPI.py
@@ -14,38 +14,23 @@ def index():
 # MAKE API GET CALL + Load data from csv 
 @app.route('/cheapest', methods=['GET'])
 def load_data():
-    # return "TRUE"
     listings = []
 
     try:
-        print("TEST1")
         file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'listings.csv')
         with open(file_path, "r") as csv_file:
-            print("TEST2")
             lines = csv.DictReader(csv_file)
             for line in lines:
                 listings.append(line)
             return listings
-
     except FileNotFoundError:
         return jsonify({"Err: File not exists"}), 404
-
     
     for listing in listings:
-        print(listing)
-        try:
-            listing["Cost (AUD)"] = float(listing["Cost (AUD)"].replace(',', ''))
-        except ValueError:
-            listing["Cost (AUD)"] = float('inf')
+        listing['price'] = float(listing['price'].replace('AUD ', ''))
+        
+    cheapest_listings = sorted(listings, key=lambda x: x['price'])[:50]
     
-    cheapest_listings = sorted(listings, key=lambda x: x["Cost (AUD)"])[:50]
-
-    for listing in cheapest_listings:
-        if listing["Cost (AUD)"] != float('inf'):
-            listing["Cost (AUD)"] = str(listing["Cost (AUD)"])
-        else:
-            listing["Cost (AUD)"] = "NA"
-
     return jsonify(cheapest_listings)
 
 
